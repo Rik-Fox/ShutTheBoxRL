46bc05227635e7206771554dc115f936fba227b8
Rik-Fox <richard.fox@warwick.ac.uk>
2023-03-22 13:35:48 +0000

    remove unused custom logging

--- main.py
+++ main.py
@@ -10,16 +10,22 @@ from stable_baselines3 import DQN
 
 from STBgym import ShutTheBoxEnv
 
-# from custom_logging import CustomTrackingCallback
-
 
 def Main(args):
     wkdir = os.path.dirname(os.path.abspath(__file__))
-    cp_log_path = os.path.join(wkdir, f"checkpoints/{args.net_arch}_{args.model_name}/")
-    monitor_dir = os.path.join(wkdir, f"monitor/{args.net_arch}_{args.model_name}")
-    tb_log_path = os.path.join(wkdir, f"tb_logs/{args.net_arch}_{args.model_name}")
+    # build the architecture string
+    arch_str = ""
+    for layer in range(len(args.net_arch)):
+        arch_str += f"_{args.net_arch[layer]}"
+    # remove the first underscore
+    arch_str = arch_str[1:]
 
-    # Create a callback for evaluation during training
+    # set up the paths for the checkpoints, monitor and tensorboard logs
+    cp_log_path = os.path.join(wkdir, "checkpoints", f"{arch_str}_{args.model_name}/")
+    monitor_dir = os.path.join(wkdir, "monitor", f"{arch_str}_{args.model_name}")
+    tb_log_path = os.path.join(wkdir, "tb_logs", f"{arch_str}_{args.model_name}")
+
+    # Create a callback for checkpoints during training
     callbacks = [
         clbks.CheckpointCallback(
             10_000,
@@ -30,6 +36,7 @@ def Main(args):
         clbks.StopTrainingOnMaxEpisodes(1_000_000_000, verbose=1),
     ]
 
+    # Create the environment
     env = make_vec_env(
         ShutTheBoxEnv,
         n_envs=200,
@@ -37,16 +44,17 @@ def Main(args):
         # vec_env_cls=SubprocVecEnv,
         # vec_env_kwargs={"start_method": "fork"},
     )
-    # env = Monitor(env, monitor_dir)
+
+    # Load the model if it exists
     try:
-        m_s = os.listdir(os.path.join(wkdir, "final_model/"))
+        m_s = os.listdir(os.path.join(wkdir, "final_model"))
         latest_m = np.max([int(m.split("_")[0]) for m in m_s])
 
         load_path = os.path.join(
-            wkdir, f"final_model/{latest_m}_{args.net_arch}_{args.model_name}"
+            wkdir, "final_model", f"{latest_m}_{arch_str}_{args.model_name}"
         )
         save_path = os.path.join(
-            wkdir, f"final_model/{latest_m+1}_{args.net_arch}_{args.model_name}"
+            wkdir, "final_model", f"{latest_m+1}_{arch_str}_{args.model_name}"
         )
 
         model = DQN.load(load_path, env=env)
@@ -55,37 +63,39 @@ def Main(args):
         # not sure if actually needed but I have
         # had to do this before so better safe than sorry
         model._episode_num = model._episode_num
-        model._n_calls = model._n_calls
+        model._n_calls = model._n_calls  # number of calls to learn()
         model._n_updates = model._n_updates
         model.num_timesteps = model.num_timesteps
         model.replay_buffer = model.replay_buffer
-        model.learning_starts = 0
+        model.learning_starts = 0  # start learning immediately
         # model.load_replay_buffer(model.replay_buffer)
         model.exploration_schedule = utils.get_linear_fn(
             model.exploration_final_eps,
             model.exploration_final_eps,
             0,
         )
+    # if the model does not exist, create a new one
     except FileNotFoundError:
+        # save path for the final model
         save_path = os.path.join(
-            wkdir, f"final_model/{0}_{args.net_arch}_{args.model_name}"
+            wkdir, "final_model", f"{0}_{arch_str}_{args.model_name}"
         )
 
         model = DQN(
             "MlpPolicy",
             env,
             learning_rate=1e-4,
-            buffer_size=1_000_000,  # 1e6
-            learning_starts=50_000,
+            buffer_size=1_000_000,  #  replay buffer size
+            learning_starts=50_000,  # number of steps before learning starts
             batch_size=1024,
             gamma=1.0,
             train_freq=64,
             gradient_steps=1,
-            policy_kwargs={"net_arch": [128, 256, 128]},
+            policy_kwargs={"net_arch": args.net_arch},  # NN architecture
             replay_buffer_class=None,
             replay_buffer_kwargs=None,
             optimize_memory_usage=False,
-            target_update_interval=10_000,
+            target_update_interval=10_000,  # update the target network every 10_000 steps
             exploration_fraction=0.1,
             exploration_initial_eps=1.0,
             exploration_final_eps=0.05,
@@ -116,8 +126,8 @@ if __name__ == "__main__":
     param_parser.add_argument(
         "--net_arch",
         nargs="+",
-        type=str,
-        default=None,
+        type=list,
+        default=[128, 256, 128],
         help="List of NN layers to be built for a model",
     )
     param_parser.add_argument(
